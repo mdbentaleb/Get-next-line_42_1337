d62d4309688a599d619415049ebe272a0455b4af
Mohamed Ben taleb <moben-ta@c2r5p3.1337.ma>
2024-11-15 12:13:53 +0100

    -

--- get_next_line.c
+++ get_next_line.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #define BUFFER_SIZE 42 
 
-char *get_next_line(int fd)
+char read_to_newline(int fd, char **remaind)
 {
-    char buffer[BUFFER_SIZE];
+    char buffer[BUFFER_SIZE + 1];
     ssize_t bytes_read;
     char *line = NULL;
     size_t line_len = 0;
@@ -20,24 +18,34 @@ char *get_next_line(int fd)
 
         //search if theres a \n
         char *new_line_search = ft_strchr(buffer, '\n');
+        if (new_line_search)
+        {
+            *new_line_search = '\0';
+            *remaind = ft_strdu(new_line_search + 1);
+        }
+
+        line = ft_strjoin(line, buffer);
+        if (*remaind)
+            break;
     }
-
-
-    return bytes_read;
+    return line;
 }
 
+char *get_next_line(int fd) {
+    static char *remaind;
+    char *line;
 
-// int main()
-// {
-//     int fd = open("med.txt", O_RDONLY);
-//     if (fd == -1) {
-//         perror("Failed to open file");
-//         return 1;
-//     }
+    if (fd < 0 || BUFFER_SIZE <= 0) {
+        return NULL;
+    }
+    
+    if (remaind) {
+        line = ft_strdup(remaind);
+        free(remaind);
+        remaind = NULL;
+    } else {
+        line = read_until_newline(fd, &remaind);
+    }
 
-//     printf("%s", get_next_line(fd));
-
-//     close(fd);
-
-//     return 0;
-// }
+    return line;
+}
