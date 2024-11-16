7c171166c557a004c5e77b524c6522399f025e29
Mohamed Ben taleb <moben-ta@c2r5p3.1337.ma>
2024-11-16 17:01:34 +0100

    -

--- get_next_line.h
+++ get_next_line.h
@@ -11,6 +11,8 @@
 char    *get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
-
+int	check_new_line(char *line);
+char	*after_new_line(char *line);
+char	*before_new_line(char *line);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -99,3 +99,31 @@ char	*before_new_line(char *line)
 	free(line);
 	return (tmp);
 }
+// char	*ft_strjoin(char *s1, char *s2)
+// {
+// 	int		j;
+// 	int		count;
+// 	char	*p;
+// 	int		len_s1;
+// 	int		len_s2;
+
+// 	j = 0;
+// 	count = 0;
+// 	len_s1 = ft_strlen(s1);
+// 	len_s2 = ft_strlen(s2);
+// 	if (len_s1 == 0 && len_s2 == 0)
+// 		return (0);
+// 	p = malloc(len_s1 + len_s2 + 1);
+// 	if (!p)
+// 		return (0);
+// 	while (count < len_s1)
+// 	{
+// 		p[count] = s1[count];
+// 		count++;
+// 	}
+// 	while (count < len_s1 + len_s2)
+// 		p[count++] = s2[j++];
+// 	p[count] = '\0';
+// 	free(s1);
+// 	return (p);
+// }
